7e12c8ef32e966564d6279c034f178c4aa01b72e
Techsupportman <ayanbalajik@gmail.com>
2024-05-19 01:48:31 +0000

    oh god

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -23,6 +23,12 @@
     import type { PageData } from "./$types.js";
   import SettingsForm from "./settings/settings-form.svelte";
   export let data: PageData;
+  import Sun from "lucide-svelte/icons/sun";
+  import Moon from "lucide-svelte/icons/moon";
+ 
+  import { toggleMode } from "mode-watcher";
+  import { Switch } from "$lib/components/ui/switch";
+  import { Label } from "$lib/components/ui/label";
   </script>
    
 
@@ -64,5 +70,15 @@
 <br>
 <br>
 
+<div class="flex items-center space-x-2">
+  <Button on:click={toggleMode} variant="outline" size="icon">
+    <Sun
+      class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+    />
+    <Moon
+      class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+    />
+    <span class="sr-only">Toggle theme</span>
+  </Button>
 
-<SettingsForm data={data.form} />
+ </div>
