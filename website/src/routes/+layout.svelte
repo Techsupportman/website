834f86316e4cafe486770dbe92578a539cc9186f
Techsupportman <ayanbalajik@gmail.com>
2024-06-03 00:10:26 +0000

    yeeees

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -1,8 +1,48 @@
-<script>
+  <script lang="ts">
     import "../app.css";
-    import { ModeWatcher } from "mode-watcher";
+    import SidebarNav from "$lib/components/sidebar-nav.svelte";
+    import { Separator } from "$lib/components/ui/separator/index.js";
+  
+  
+    const sidebarNavItems = [
+      {
+        title: "Profile",
+        href: "/profile",
+      },
+      {
+        title: "Account",
+        href: "/account",
+      },
+      {
+        title: "Appearance",
+        href: "/appearance",
+      },
+      {
+        title: "Notifications",
+        href: "/notifications",
+      },
+      {
+        title: "Display",
+        href: "/display",
+      },
+    ];
   </script>
-   
-   <ModeWatcher />
-  <slot />
+  
 
+  <div class="hidden space-y-6 p-10 pb-16 md:block">
+    <div class="space-y-0.5">
+      <h2 class="text-2xl font-bold tracking-tight">Settings</h2>
+      <p class="text-muted-foreground">
+        Manage your account settings and set e-mail preferences.
+      </p>
+    </div>
+    <Separator class="my-6" />
+    <div class="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+      <aside class="-mx-4 lg:w-1/5">
+        <SidebarNav items={sidebarNavItems} />
+      </aside>
+      <div class="flex-1 lg:max-w-2xl">
+        <slot />
+      </div>
+    </div>
+  </div>
--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -1,84 +1,15 @@
-<h1 class="text-red-500">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
 <script lang="ts">
-    import CalendarIcon from "lucide-svelte/icons/calendar";
-    import {
-      DateFormatter,
-      type DateValue,
-      getLocalTimeZone
-    } from "@internationalized/date";
-    import { cn } from "$lib/utils.js";
-    import { Button } from "$lib/components/ui/button/index.js";
-    import { Calendar } from "$lib/components/ui/calendar/index.js";
-    import * as Popover from "$lib/components/ui/popover/index.js";
-    import { Toaster, toast } from 'svelte-sonner';
-   
-    const df = new DateFormatter("en-US", {
-      dateStyle: "long"
-    });
-   
-    let value: DateValue | undefined = undefined;
+	import type { PageData } from "./$types.js";
+	import ProfileForm from "./profile/profile-form.svelte";
+	import { Separator } from "$lib/components/ui/separator/index.js";
+	export let data: PageData;
+</script>
 
-    import type { PageData } from "./$types.js";
-  import SettingsForm from "./settings/settings-form.svelte";
-  export let data: PageData;
-  import Sun from "lucide-svelte/icons/sun";
-  import Moon from "lucide-svelte/icons/moon";
- 
-  import { toggleMode } from "mode-watcher";
-  import { Switch } from "$lib/components/ui/switch";
-  import { Label } from "$lib/components/ui/label";
-  </script>
-   
-
-
-
-  <Popover.Root>
-    <Popover.Trigger asChild let:builder>
-      <Button
-        variant="outline"
-        class={cn(
-          "w-[280px] justify-start text-left font-normal",
-          !value && "text-muted-foreground"
-        )}
-        builders={[builder]}
-      >
-        <CalendarIcon class="mr-2 h-4 w-4" />
-        {value ? df.format(value.toDate(getLocalTimeZone())) : "Pick a date"}
-      </Button>
-    </Popover.Trigger>
-    <Popover.Content class="w-auto p-0">
-      <Calendar bind:value initialFocus />
-    </Popover.Content>
-  </Popover.Root>
-   
-   <br>
-   <br>
-
-  <Button>Click me</Button>
-
-  <br>
-<br>
-
-
-
-<Toaster />
-<Button on:click={() => toast.success("Hello world")}>Show toast</Button>
-
-
-<br>
-<br>
-
-<div class="flex items-center space-x-2">
-  <Button on:click={toggleMode} variant="outline" size="icon">
-    <Sun
-      class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-    />
-    <Moon
-      class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-    />
-    <span class="sr-only">Toggle theme</span>
-  </Button>
-
- </div>
+<div class="space-y-6">
+	<div>
+		<h3 class="text-lg font-medium">Profile</h3>
+		<p class="text-sm text-muted-foreground">This is how others will see you on the site.</p>
+	</div>
+	<Separator />
+	<ProfileForm data={data.form} />
+</div>
